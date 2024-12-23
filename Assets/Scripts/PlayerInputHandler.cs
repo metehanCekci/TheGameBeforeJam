82aae3b850665cac8873ce4a2cec52f7c4e73781
DeenizAcar <146178956+DeenizAcar@users.noreply.github.com>
2024-12-23 23:38:10 +0300

    a

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -1,7 +1,6 @@
-using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
+
 public class PlayerInputHandler : MonoBehaviour
 {
     [Header("Input Actions Asset")]
@@ -10,59 +9,74 @@ public class PlayerInputHandler : MonoBehaviour
     [SerializeField] private string mapName = "Player";
     [Header("Actions Name Reference")]
     [SerializeField] private string move = "Move";
+    [Header("Actions Name Reference")]
     [SerializeField] private string look = "Look";
+    [Header("Actions Name Reference")]
     [SerializeField] private string jump = "Jump";
+    [Header("Actions Name Reference")]
     [SerializeField] private string sprint = "Sprint";
 
     [Header("Movement Joystick")]
     [SerializeField] private TouchJoystick movementJoystick;
     [SerializeField] private bool isJoystickActive = false;
+
     private InputAction moveAction;
     private InputAction lookAction;
     private InputAction jumpAction;
     private InputAction sprintAction;
-    public static PlayerInputHandler Instance{get; private set;}
 
-    public Vector2 MoveInput {get; private set;}
-    public Vector2 LookInput {get; private set;}
-    public bool JumpTriggered {get; private set;}
-    public float SprintValue {get; private set;}
+    public static PlayerInputHandler Instance { get; private set; }
 
-    
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 LookInput { get; private set; }
+    public bool JumpTriggered { get; private set; }
+    public float SprintValue { get; private set; }
 
-    void Awake(){
-        
-        if(Instance==null){
+    private bool isMobile;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
             Instance = this;
         }
-
 
         moveAction = playerControls.FindActionMap(mapName).FindAction(move);
         lookAction = playerControls.FindActionMap(mapName).FindAction(look);
         jumpAction = playerControls.FindActionMap(mapName).FindAction(jump);
         sprintAction = playerControls.FindActionMap(mapName).FindAction(sprint);
+
         RegisterInputActions();
+
+        isMobile = Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer;
+        isJoystickActive = isMobile;
     }
 
-    void Start(){
-        
-        
+    void Start()
+    {
         movementJoystick = GameObject.FindWithTag("MoveJoystick").GetComponent<TouchJoystick>();
+
+        if (movementJoystick != null)
+        {
+            movementJoystick.gameObject.SetActive(isJoystickActive);
+        }
+
         Application.targetFrameRate = 120;
     }
 
-    
-
-    void Update(){
-        if(isJoystickActive){
+    void Update()
+    {
+        if (isJoystickActive)
+        {
             Vector2 joystickInput = movementJoystick.GetJoystickInput();
-            if(joystickInput.magnitude>0) MoveInput = joystickInput;
+            if (joystickInput.magnitude > 0) MoveInput = joystickInput;
             else MoveInput = Vector2.zero;
         }
     }
 
-    void RegisterInputActions(){
-        moveAction.performed += context => MoveInput = context.ReadValue<Vector2>(); 
+    void RegisterInputActions()
+    {
+        moveAction.performed += context => MoveInput = context.ReadValue<Vector2>();
         moveAction.canceled += context => MoveInput = Vector2.zero;
 
         lookAction.performed += context => LookInput = context.ReadValue<Vector2>();
@@ -75,20 +89,19 @@ public class PlayerInputHandler : MonoBehaviour
         sprintAction.canceled += context => SprintValue = 0f;
     }
 
-    void OnEnable(){
+    void OnEnable()
+    {
         moveAction.Enable();
         lookAction.Enable();
         jumpAction.Enable();
         sprintAction.Enable();
     }
-    void OnDisable(){
+
+    void OnDisable()
+    {
         moveAction.Disable();
         lookAction.Disable();
         jumpAction.Disable();
         sprintAction.Disable();
     }
-
-    
-
-    
 }
