106917d9f246eb9b67a101202bfd8f667152d6af
Metehan <metehancekci9@gmail.com>
2024-12-24 23:09:08 +0300

    pc gun rotation fixes

---
+++ Assets/Scripts/GunFlipScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class GunFlipScript : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.localRotation.eulerAngles.z > 90 && gameObject.transform.localRotation.eulerAngles.z < 270)
+        {
+            Debug.Log("silah çevirildi");
+            spriteRenderer.flipY = true;
+        }
+        else
+        {
+            spriteRenderer.flipY = false;
+        }
+    }
+}
